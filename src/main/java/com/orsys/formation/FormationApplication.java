22a76f3864043e1165e7bd127ca9ce983f0874e7
Mourad* <elmoromourad.code@gmail.com>
2025-07-09 16:28:33 +0100

    v1 of the app

--- src/main/java/com/orsys/formation/FormationApplication.java
+++ src/main/java/com/orsys/formation/FormationApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FormationApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello woooorld");
+
 		SpringApplication.run(FormationApplication.class, args);
 	}
 
